0ab40a44ded690e3f88c3f1bf52491b0fc4dd831
Caloy e ako <kingcort01.office@gmail.com>
2016-09-27 13:59:45 +0800

    routing phase 2

--- src/app/receipt/receipt.component.ts
+++ src/app/receipt/receipt.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { OrderService } from '../order.service'
 
 @Component({
   selector: 'app-receipt',
@@ -7,12 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ReceiptComponent implements OnInit {
 
-  @Input()
   order
 
-  constructor() {
-    
-   }
+  constructor(od:OrderService) {
+    this.order = od.getAllOrder()[0]
+  }
 
   ngOnInit() {
   }
